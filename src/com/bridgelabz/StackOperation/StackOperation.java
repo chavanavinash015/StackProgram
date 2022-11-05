5ac4655e1b80b85240e6e9dbac4bde30651a8f63
arc-015 <chavanavinash015@gmail.com>
2022-11-05 10:18:26 +0530

    Modify: Welcome to the Stack Operation

---
+++ src/com/bridgelabz/StackOperation/StackOperation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.StackOperation;
+
+public class StackOperation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to stack operation ");
+
+    }
+}
